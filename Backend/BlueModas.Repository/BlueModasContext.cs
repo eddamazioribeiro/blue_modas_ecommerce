4f3c72c1f37a250fdb2bce39974b741ce2910568
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-07-01 21:56:51 -0300

    Created models and corresponding DbSets on context.

--- Backend/BlueModas.Repository/BlueModasContext.cs
+++ Backend/BlueModas.Repository/BlueModasContext.cs
@@ -1,9 +1,18 @@
+using BlueModas.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlueModas.Repository
 {
     public class BlueModasContext : DbContext
     {
-        
+        public BlueModasContext(DbContextOptions<BlueModasContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
