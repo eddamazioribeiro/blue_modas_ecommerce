d6575bdc341bcc47c2da2dddca09de2b462a687d
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-07-04 20:29:17 -0300

    Created navbar component on frontend app.

--- Frontend/BlueModas-App/src/app/app.module.ts
+++ Frontend/BlueModas-App/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ProductsComponent
+      ProductsComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
