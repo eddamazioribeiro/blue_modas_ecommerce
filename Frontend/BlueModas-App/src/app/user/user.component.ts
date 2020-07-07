251b742b1d8db1d673441519c0492632d5626992
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-07-07 06:07:29 -0300

    Generate order.

--- Frontend/BlueModas-App/src/app/user/user.component.ts
+++ Frontend/BlueModas-App/src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, FormGroup, FormControl } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -23,8 +24,10 @@ export class UserComponent implements OnInit {
   });
 
   user: any = {};
+  address: any = {};
+  order: any = {};
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
@@ -72,13 +75,82 @@ export class UserComponent implements OnInit {
     }
 
     this.http.post<any>(
-      "http://localhost:5000/api/address/7", address, httpOptions).subscribe(
+      'http://localhost:5000/api/address/7', address, httpOptions).subscribe(
         data => {
-          console.log(data);
+          this.address = data;
+          this.createOrder();
         },
         err => {
           console.error(err, address);
         }
       );
+  }
+
+  private createOrder() {
+    let order = {
+      userId: this.user.id,
+      payment: null,
+      status: 0,
+      shippingAddressId: parseInt(this.address.id)
+    };
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+
+    return this.http.post<any>(
+      'http://localhost:5000/api/order', order, httpOptions).subscribe(
+        data => {
+          this.order = data;
+          this.saveOrderItems(data.id);
+        },
+        err => {
+          console.error(err, order);
+        }
+      );
+  }
+
+  private saveOrderItems(orderId) {
+    let products: any = this.getProductsInCart();
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    products.forEach(p => {
+      let item = {
+        orderId: orderId,
+        productId: p.id,
+        price: parseFloat(p.id),
+        quantity: parseInt(p.quantity)
+      }
+      item.orderId = orderId;
+
+      this.http.post<any>(
+        'http://localhost:5000/api/orderitem', item, httpOptions).subscribe(
+          data => {
+            console.log('created item', item);
+          },
+          err => {
+            console.error('erro', item);
+          }
+        );
+    });
+  }
+
+  public getProductsInCart(): [] {
+    let products = JSON.parse(localStorage.getItem('products'));
+
+    console.log(products);
+
+    if (products) {
+      return products;
+    }
+
+    return [];
   }
 }
